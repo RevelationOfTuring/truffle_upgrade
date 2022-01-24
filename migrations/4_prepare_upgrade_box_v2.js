d9ff1a81f437c5a6d1b2ce25ba5a4243e112f6fd
MichaelWang <wangguanyi@web3.com>
2022-01-24 19:02:04 +0800

    4_prepare_upgrade_box_v2.js

---
+++ migrations/4_prepare_upgrade_box_v2.js
@@ -0,0 +1,10 @@
+const Box = artifacts.require('Box')
+const BoxV2 = artifacts.require('BoxV2')
+
+const {prepareUpgrade} = require('@openzeppelin/truffle-upgrades')
+
+module.exports = async function (deployer) {
+    const box = await Box.deployed()
+    console.log('original address: ' + box.address)
+    await prepareUpgrade(box.address, BoxV2, {deployer})
+}
