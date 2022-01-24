e9b9c5aa3d0d61ed8c01dfe207a26fc9c78078de
MichaelWang <wangguanyi@web3.com>
2022-01-24 19:24:35 +0800

    annotation

--- migrations/4_prepare_upgrade_box_v2.js
+++ migrations/4_prepare_upgrade_box_v2.js
@@ -7,4 +7,5 @@ module.exports = async function (deployer) {
     const box = await Box.deployed()
     console.log('original address: ' + box.address)
     await prepareUpgrade(box.address, BoxV2, {deployer})
+    // then upgrade the impl on https://gnosis-safe.io/app/rin:0xaDDE3eFb10dC32e638F70669e8370E10BB96617f/apps?appUrl=https%3A%2F%2Fcloudflare-ipfs.com%2Fipfs%2FQmQovvfYYMUXjZfNbysQDUEXR8nr55iJRwcYgJQGJR7KEA
 }
